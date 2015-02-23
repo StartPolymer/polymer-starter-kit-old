5c70f529f89287c0ae8c64d6f64370a7eaa51d87
Josef Jezek <josef@jezek.co.cz>
2015-02-23 18:18:16 +0100

    Fix images task - Optimize Images Recursively
    https://github.com/google/web-starter-kit/issues/579#issuecomment-75457434

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,15 +1,16 @@
 'use strict';
 
-// Optimize Images
+// Optimize Images Recursively (All Files)
 module.exports = function (gulp, plugins, config) { return function () {
   return gulp.src('app/images/**/*')
-    .pipe(plugins.cache(plugins.imagemin({
+    .pipe(plugins.if(plugins.if.isFile, plugins.cache(plugins.imagemin({
       progressive: true,
       interlaced: true,
       // don't remove IDs from SVGs, they are often used
       // as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
-    })))
+    }))
+    .on('error', function(err){ console.log(err); this.end; })))
     .pipe(gulp.dest('dist/images'))
     .pipe(plugins.size({title: 'images'}));
 };};

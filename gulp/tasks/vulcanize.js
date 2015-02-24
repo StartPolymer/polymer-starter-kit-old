6e1f7decc82e4235b1743c39063af25036109d1e
Josef Jezek <josef@jezek.co.cz>
2015-02-25 00:40:08 +0100

    Fix vulcanize - add csp and inline option
    https://github.com/yeoman/generator-polymer/issues/151

--- gulp/tasks/vulcanize.js
+++ gulp/tasks/vulcanize.js
@@ -5,7 +5,9 @@ module.exports = function (gulp, plugins, config) { return function () {
   return gulp.src('dist/elements/elements.vulcanized.html')
     .pipe(plugins.vulcanize({
       dest: 'dist/elements',
-      strip: true
+      csp: true,    // Extract inline <script> blocks into a separate file.
+      inline: true, // Inline all scripts and stylesheets.
+      strip: true   // Strip comments and empty text nodes from output.
     }))
     .pipe(gulp.dest('dist/elements'))
     // Revving file

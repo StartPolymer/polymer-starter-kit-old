0f21b516769331f8c9b79f1f1e12403cf8201791
Josef Jezek <josef@jezek.co.cz>
2015-03-02 16:23:10 +0100

    Fix image task
    https://github.com/google/web-starter-kit/issues/579#issuecomment-75692699

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -3,14 +3,14 @@
 // Optimize Images Recursively (All Files)
 module.exports = function (gulp, plugins, config) { return function () {
   return gulp.src('app/images/**/*')
-    .pipe(plugins.if(plugins.if.isFile, plugins.cache(plugins.imagemin({
+    .pipe(plugins.cache(plugins.imagemin({
       progressive: true,
       interlaced: true,
       // don't remove IDs from SVGs, they are often used
       // as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
     }))
-    .on('error', function(err){ console.log(err); this.end; })))
+    .on('error', function(err){ console.log(err); this.end; }))
     .pipe(gulp.dest('dist/images'))
     .pipe(plugins.size({title: 'images'}));
 };};

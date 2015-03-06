419e3bb7208f3064de555d8d6b7a565838f4d371
Josef Jezek <josef@jezek.co.cz>
2015-03-06 18:36:27 +0100

    Disable jshint

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,10 @@ gulp.task('default', ['clean'], function (cb) {
   require('run-sequence')(
 //    'views',
     ['copy', 'styles'],
-    ['jshint', 'images', 'fonts', 'html', 'styles:elements'],
+    [
+//      'jshint',
+      'images', 'fonts', 'html', 'styles:elements'
+    ],
     'vulcanize',
     'revreplace',
 //    'gzip',
